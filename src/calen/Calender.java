e9e8c2c0010d333cc570898b74b2d42a1838eb80
yeomim <yeomim@livingroom_pc>
2017-07-20 16:25:21 +0900

    특정 달의 최대일 수 출력하기

--- src/calen/Calender.java
+++ src/calen/Calender.java
@@ -1,5 +1,7 @@
 package calen;
 
+import java.util.Scanner;
+
 public class Calender {
 	public static void main(String[] args) {
 
@@ -10,5 +12,16 @@ public class Calender {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		System.out.println("29 30 31");
+		
+//		숫자를 입력받아 해당하는 달의 최대일 수를 출력하느 프로그램
+		System.out.println("원하는 달을 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		int month =scanner.nextInt();
+		
+		int maxDayz []  ={31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.printf("%d월은 %d까지 있습니다\n",month,maxDayz[month-1]);
+		
+		
+		
 	}
 }
