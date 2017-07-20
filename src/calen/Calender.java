195a0ed38d9e58d906d565af031ab7fc66058982
yeomim <yeomim@livingroom_pc>
2017-07-20 17:41:48 +0900

    for문을 이용하여 반복횟수 정하기

--- src/calen/Calender.java
+++ src/calen/Calender.java
@@ -8,28 +8,24 @@ public class Calender {
 	public int getMaxDaysOfMonth(int month) {
 		// return MAX_DAYS[month-1];
 
-//		switch (month) {
-//		case 2:
-//			return 28;
-//		case 4:
-//			return 30;d
-//		default:
-//			return 31;
-//		}
-		
+		// switch (month) {
+		// case 2:
+		// return 28;
+		// case 4:
+		// return 30;d
+		// default:
+		// return 31;
+		// }
 
-		if(month ==1){
+		if (month == 1) {
 			return 31;
-		}else if(month ==2)
-		{return 28;}else{
-			
-		return 30;}
-		}
-		
-		
-	
-		
+		} else if (month == 2) {
+			return 28;
+		} else {
 
+			return 30;
+		}
+	}
 
 	public void printSampleCalendar() {
 		System.out.println("--------------------");
@@ -45,15 +41,27 @@ public class Calender {
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대일 수를 출력하느 프로그램
-		System.out.println("원하는 달을 입력하세요");
-		Scanner scanner = new Scanner(System.in);
-		Calender cal = new Calender();
+		// System.out.println("원하는 달을 입력하세요");
+		  Scanner scanner = new Scanner(System.in);
+		  Calender cal = new Calender();
 
-		int month = scanner.nextInt();
+		System.out.println("반복횟수를 입력하세요");
 
+		int repeat = scanner.nextInt();
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			// int maxDayz [] ={31,28,31,30,31,30,31,31,30,31,30,31};
+			System.out.printf("%d월은 %d까지 있습니다\n", month, cal.getMaxDaysOfMonth(month));
+			cal.printSampleCalendar();
+		}
+		System.out.println("bye~~");
+		scanner.close();
 		// int maxDayz [] ={31,28,31,30,31,30,31,31,30,31,30,31};
-		System.out.printf("%d월은 %d까지 있습니다\n", month, cal.getMaxDaysOfMonth(month));
-		cal.printSampleCalendar();
+		// System.out.printf("%d월은 %d까지 있습니다\n", month,
+		// cal.getMaxDaysOfMonth(month));
+		// cal.printSampleCalendar();
 
+		
 	}
 }
